1ac0468d6881d1b305a663468c30530bde87c073
James Truitt <jamestruitt@mac.com>
2017-06-08 01:02:24 -0400

    update SQL connect string

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', slotEnvironment:process.env.slotEnvironment, slotSQLConnection:process.env.defaultConnection });
+  res.render('index', { title: 'Express', slotEnvironment:process.env.slotEnvironment, slotSQLConnection:process.env.SQLCONNSTR_defaultConnection });
 });
 
 module.exports = router;
