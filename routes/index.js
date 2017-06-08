9f7ef18ad3f00a7580ad4bc9c4221367c5964a9b
James Truitt <jamestruitt@mac.com>
2017-06-08 11:27:44 -0400

    console logging

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 
     console.log('***************************************************');
-    console.log('Current Time:      ' + currentTime:new Date().getTime() / 1000 })
-    console.log('Environment:       ' + process.env.slotEnvironment);
-    console.log('DB Connect String: ' + process.env.SQLCONNSTR_defaultConnection);
+   // console.log('Current Time:      ' & new Date().getTime() / 1000);
+    console.log('Environment:       ' & process.env.slotEnvironment);
+    console.log('DB Connect String: ' & process.env.SQLCONNSTR_defaultConnection);
     console.log('***************************************************');
 
   res.render('index', { title: 'Slot Demo', slotEnvironment:process.env.slotEnvironment, slotSQLConnection:process.env.SQLCONNSTR_defaultConnection, currentTime:new Date().getTime() / 1000 });
