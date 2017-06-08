1a7b93d63d2de5ef3a3e2831a2e1c36893ff6e3f
James Truitt <jamestruitt@mac.com>
2017-06-08 10:48:13 -0400

    config

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  console.log(slotEnvironment:process.env.slotEnvironment);
+  console.debug(process.env.slotEnvironment);
   console.log(process.env.SQLCONNSTR_defaultConnection);
 
   res.render('index', { title: 'Slot Demo', slotEnvironment:process.env.slotEnvironment, slotSQLConnection:process.env.SQLCONNSTR_defaultConnection });
