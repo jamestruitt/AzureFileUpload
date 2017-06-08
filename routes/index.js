43a68c7ec6fbd31290b0bd660d56a2bea83d7f88
James Truitt <jamestruitt@mac.com>
2017-06-08 00:07:06 -0400

    updated for config values

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', slotEnvironment:process.env.slotEnvironment, slotSQLConnection:process.env.defaultConnection });
 });
 
 module.exports = router;
